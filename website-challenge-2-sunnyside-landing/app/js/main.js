7b8844fd010d58509cf59b27aec2daa806eaf62d
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-03-16 19:24:11 +0100

    refactor: moved mobile menu to nav, close on blur

--- website-challenge-2-sunnyside-landing/app/js/main.js
+++ website-challenge-2-sunnyside-landing/app/js/main.js
@@ -4,3 +4,8 @@ function toggleMobileNav() {
 	mobileNav.classList.replace(`mobile-nav--${mobileNavState ? "open" : "closed"}`, `mobile-nav--${!mobileNavState ? "open" : "closed"}`);
 	mobileNavState = !mobileNavState;
 }
+
+window.addEventListener("click", e => {
+	if(mobileNavState && !e.target.classList.contains("mobile-nav") && !document.querySelector("#hamburger").contains(e.target))
+		toggleMobileNav();
+});
